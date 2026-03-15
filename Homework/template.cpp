8c1780d4b62ec93a227322c63bdc496b6ff45159
Lucy Blanks <LLBlanks06@gmail.com>
2026-03-15 13:58:14 -0700

    template for all assignments, homework, and projects!

---
+++ Homework/template.cpp
@@ -0,0 +1,32 @@
+/*
+Author: Lucy Blanks
+Date:
+Purpose: template to be used for all future homework, labs, exams, projects
+*/
+
+//system libraries
+#include <iostream> //input/output library
+using namespace std; //using namepace standard
+
+//user libaries
+
+// global constant not variable
+//science, math, conversion, dimension
+
+//function prototypes
+
+//execution begins here at main
+int main( int argc, char **argv ) {
+    //set random number seed
+
+    //declare variables
+
+    //map/prcess the inputs -> outputs
+
+    //display input/outputs
+    
+    //clean up memory, close files
+
+    //exit the program
+    return 0;
+}
