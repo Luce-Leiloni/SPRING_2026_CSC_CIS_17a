45bf085bec2e43972cb713fb76b23782e6a73d8f
Lucy Blanks <LLBlanks06@gmail.com>
2026-06-14 03:15:43 -0700

    Create sudoku.h

---
+++ Projects/Project2/Sudoku_v3/sudoku.h
@@ -0,0 +1,52 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
+#include <vector>
+#include <string>
+using namespace std;
+
+class Sudoku {
+    protected: 
+        vector<vector<int>> solved;                 //initial board
+        vector<vector<int>> puzzle;                 //board that player solves that must match with solved
+        int zeroAmt;                                //amount of zeroes to remove based on difficulty
+};
+
+class SudokuBoard : public Sudoku {
+    private:
+        bool isInRow(int r, int n) const;
+        bool isInCol(int c, int n) const;
+        bool isInBox(int r, int c, int n) const;     
+        void genBoard();                            //generate board
+        void fillZero();                            //fill board with zeroes
+        void setDifficulty();
+
+
+    public:
+        SudokuBoard();                               //constructor
+        void setBoard();
+        void printBoard() const;   
+        bool correct(int r, int c, int n) const;
+        void solve(int &r, int &c, int &n);
+        
+        vector<vector<int>>& getSolved() {return solved;}
+        vector<vector<int>>& getPuzzle() {return puzzle;}
+
+        
+};
+
+class Player {
+    private:
+        string name;
+        SudokuBoard game;
+
+    public:
+        Player();
+        void getFile();
+        bool save();
+        bool load();
+        void menu(int &r, int &c, int &n);
+        void play(int &r, int &c, int &n);
+
+};
+#endif
